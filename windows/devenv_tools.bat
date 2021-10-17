05ffb17a077b7f608c5860193a9dd293671b4880
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2021-10-17 22:01:40 +0200

    Add basic tool downloader and configuration files for windows and linux

---
+++ windows/devenv_tools.bat
@@ -0,0 +1,15 @@
+@echo off
+if not exist "%~dp0bin\coreutils.exe" goto fail
+set PATH=%~dp0bin;%~dp0neovim\bin;%~dp0mingit\cmd;%PATH%
+:: doskey j=zoxide query $*
+:: doskey ff=fzf $*
+
+if not exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat" goto fail
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+goto :eof
+
+:fail
+color 4f
+echo ERROR: ¯\_(ツ)_/¯
+title ERROR
+goto :eof
