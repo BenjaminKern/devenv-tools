e2a171858bae30214f7cb5bda82f165c76c00cc4
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2021-11-14 20:27:44 +0100

    Add fzf preview bind keys

--- linux/devenv_tools.bash
+++ linux/devenv_tools.bash
@@ -4,13 +4,10 @@ devenv_tools_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PATH=$devenv_tools_dir/bin:$PATH
 source $devenv_tools_dir/config/fzf-key-bindings.bash
 export FZF_DEFAULT_COMMAND="fd --color never --type f --hidden --ignore-file $devenv_tools_dir/share/nvim/.fd-ignore"
-export GF_PREFERRED_PAGER="delta --theme=gruvbox --highlight-removed -w __WIDTH__"
-export GF_LOG_MENU_PARAMS='--pretty="%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --topo-order'
-export GF_REFLOG_MENU_PARAMS='--pretty=fuzzyformat'
 export STARSHIP_CONFIG=$devenv_tools_dir/config/starship.toml
 LS_COLORS="$(vivid generate one-dark)"
 alias ls='lsd'
-alias ff='fzf'
+alias ff="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}' --bind ctrl-alt-k:preview-page-up,ctrl-alt-j:preview-page-down"
 EDITOR=nvim
 alias cat='bat --paging=never'
 
