039cebc86e8f480b81f81366aa7bbc50937bd6ca
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2022-04-20 18:38:01 +0200

    Update window dev tools

--- windows/devenv_tools.bat
+++ windows/devenv_tools.bat
@@ -1,11 +1,11 @@
 @echo off
-if not exist "%~dp0bin\coreutils.exe" goto fail
+if not exist "%~dp0bin\busybox.exe" goto fail
 set PATH=%~dp0bin;%~dp0neovim\bin;%~dp0mingit\cmd;%PATH%
 :: doskey j=zoxide query $*
 :: doskey ff=fzf $*
 
-if not exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat" goto fail
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+if not exist "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" goto fail
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 goto :eof
 
 :fail
