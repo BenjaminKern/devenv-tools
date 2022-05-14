eb8315e86319c1057b45ef6f6e5bacf2b86dec56
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2022-05-14 13:17:01 +0200

    Add python

--- windows/get_devenv_tools.bat
+++ windows/get_devenv_tools.bat
@@ -10,6 +10,7 @@ mkdir %DESTDIR%\clink
 mkdir %DESTDIR%\clink\scripts
 mkdir %DESTDIR%\cmake
 mkdir %DESTDIR%\clangd
+mkdir %DESTDIR%\python
 
 echo "Downloading devenv_tools config..."
 curl -sL https://raw.githubusercontent.com/BenjaminKern/devenv-tools/main/windows/devenv_tools.bat -o %DESTDIR%\devenv_tools.bat
@@ -80,6 +81,9 @@ curl -sL https://github.com/starship/starship/releases/download/v1.6.3/starship-
 tar xf starship.zip -C %DESTDIR%\bin
 echo "Download lldb..."
 curl -sL https://raw.githubusercontent.com/BenjaminKern/devenv-tools/main/windows/bin/lldb-bin.tar.xz | bsdtar xfJ - --strip-components=1 -C $DESTDIR/bin
+echo "Download python..."
+curl -sL https://www.python.org/ftp/python/3.10.4/python-3.10.4-embed-amd64.zip -o python.zip
+tar xf python.zip --strip-components=1 -C %DESTDIR%\python
 echo "Setup busybox..."
 copy /y %DESTDIR%\mingit\mingw64\bin\busybox.exe %DESTDIR%\busybox\
 curl -sL https://raw.githubusercontent.com/BenjaminKern/devenv-tools/main/windows/busybox_template.bat -o %DESTDIR%\busybox_template.bat
