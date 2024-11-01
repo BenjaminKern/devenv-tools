f58673eb3ccb8e72fa17934f038abeb6dcd7142e
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2024-11-01 14:19:01 +0100

    Update lldb build instructions

--- linux/build_lldb_vscode.sh
+++ linux/build_lldb_vscode.sh
@@ -5,13 +5,13 @@ DESTDIR="$(readlink -e $1)"
 
 # git clone --depth 1 https://github.com/llvm/llvm-project
 cmake -S llvm-project/llvm -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;lldb" -G Ninja -B build-lldb
-cmake --build build-lldb --target lldb-vscode
+cmake --build build-lldb --target lldb-dap
 cmake --build build-lldb --target lldb-server
 cmake --build build-lldb --target lldb
 cp build-lldb/lib/liblldb.so* $DESTDIR
-cp build-lldb/bin/lldb-vscode $DESTDIR
+cp build-lldb/bin/lldb-dap $DESTDIR
 cp build-lldb/bin/lldb-server $DESTDIR
 cp build-lldb/bin/lldb-argdumper $DESTDIR
 cp build-lldb/bin/lldb $DESTDIR
-patchelf --set-rpath '$ORIGIN' $DESTDIR/lldb-vscode
+patchelf --set-rpath '$ORIGIN' $DESTDIR/lldb-dap
 patchelf --set-rpath '$ORIGIN' $DESTDIR/lldb
