8a9d35cb21151c9f511c83f37262bd7d2b6542fa
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2025-01-21 20:32:20 +0100

    Completely migrate to tv

--- linux/devenv_tools.zsh
+++ linux/devenv_tools.zsh
@@ -2,6 +2,7 @@ devenv_tools_dir="$(cd "$(dirname "${(%):-%N}")" &> /dev/null && pwd)"
 if [[ ! -f "$devenv_tools_dir/devenv_tools.zsh" ]]; then
     return
 fi
+export EDITOR=nvim
 
 setopt nobeep autocd
 export CLICOLOR=1
@@ -15,32 +16,15 @@ PATH=$devenv_tools_dir/bin:$devenv_tools_dir/cpptools/debugAdapters/bin:$devenv_
 export FZF_DEFAULT_COMMAND="fd --color never --type f --hidden --ignore-file $devenv_tools_dir/share/nvim/.fd-ignore"
 export STARSHIP_CONFIG=$devenv_tools_dir/config/starship.toml
 alias ff='nvim $(tv)'
+alias ffg='nvim $(tv text)'
 alias ls='lsd'
-export EDITOR=nvim
 alias cat='bat --paging=never'
-is_in_git_repo() {
-  git rev-parse HEAD > /dev/null 2>&1
-}
-gv() {
-  is_in_git_repo || return
-
-  local filter
-  if [ -n $@ ] && [ -f $@ ]; then
-    filter="-- $@"
-  fi
-
-  git lg $@ | \
-    fzf \
-      --ansi --no-sort --reverse --tiebreak=index \
-      --preview "f() { set -- \$(echo -- \$@ | grep -o '[a-f0-9]\{7\}'); [ \$# -eq 0 ] || git show --color=always --format=fuller \$1 $filter | delta --line-numbers --syntax-theme=OneHalfDark; }; f {}" \
-      --bind "alt-j:preview-down,alt-k:preview-up,q:abort" \
-      --preview-window=right:60%
-}
+alias gv='tv git-log'
+alias which='whence -p'
 alias for_all_files='fd --type f -x'
 alias find_and_replace='fd --type f -x sd'
 alias clang_format_files='fd -e h -e cpp -e c -x clang-format -i'
 
-eval "$(fzf --zsh)"
 eval "$(zoxide init --cmd j zsh)"
 eval "$(bat --completion zsh)"
 eval "$(tv init zsh)"
