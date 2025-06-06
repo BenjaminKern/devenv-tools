d2051542b97e5441d5d7631025be2b8d719e8443
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2025-06-06 12:39:40 +0200

    Add pwd alias

--- get_devenv_tools.bat
+++ get_devenv_tools.bat
@@ -44,7 +44,7 @@ curl -sL https://github.com/chrisant996/clink/releases/download/v1.7.19/clink.1.
 tar xf "%TEMP%\clink.zip" -C "%DESTDIR%\clink"
 del "%TEMP%\clink.zip"
 
-set CMDLIST=cat cksum clear cp ls mv rm base64 cut env head tail md5sum mktemp realpath readlink sha256sum sleep split tee touch whoami yes wc
+set CMDLIST=cat cksum clear cp ls mv rm base64 cut env head tail md5sum mktemp realpath readlink sha256sum sleep split tee touch whoami yes wc pwd
 
 for %%C in (%CMDLIST%) do (
     (
