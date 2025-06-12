3cb8dbb2d54ddd4de4c2f488ce321f7ea2bc9a45
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2025-06-12 10:05:11 +0200

    Build git from source - Restructuring

--- posix/build-git.sh
+++ posix/build-git.sh
@@ -1,67 +1,58 @@
 #!/usr/bin/env bash
 set -euo pipefail
+set -x
 
 # === Versions ===
 GIT_VERSION="2.49.0"
 CURL_VERSION="8.14.1"
 WOLFSSL_VERSION="5.8.0"
-ZLIB_VERSION="1.3.1"
+ZLIB_VERSION="2.2.4"
 EXPAT_VERSION="2.7.1"
 
 BUILD_DIR="$PWD/git-build"
 INSTALL_DIR="$BUILD_DIR/install"
-NUM_CPUS=$(nproc || sysctl -n hw.ncpu)
 
 # Clean start
 rm -rf "$BUILD_DIR"
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
 
-
-# TODO: use zlib-ng
-echo "==> Building zlib..."
-curl -LO "https://zlib.net/zlib-${ZLIB_VERSION}.tar.gz"
-tar xf "zlib-${ZLIB_VERSION}.tar.gz"
-cd "zlib-${ZLIB_VERSION}"
-cmake -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DBUILD_SHARED_LIBS=OFF
-cmake --build build
-cmake --install build
-cd ..
+echo "==> Building zlib-ng..."
+mkdir -p zlib-src
+curl -sL "https://github.com/zlib-ng/zlib-ng/archive/refs/tags/${ZLIB_VERSION}.tar.gz" | tar xfz - --strip=1 -C zlib-src
+cmake -S zlib-src -Bbuild-zlib -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DBUILD_SHARED_LIBS=OFF -DZLIB_COMPAT=ON -DZLIB_ENABLE_TESTS=OFF -DZLIBNG_ENABLE_TESTS=OFF
+cmake --build build-zlib
+cmake --install build-zlib
 
 echo "==> Building expat..."
-curl -LO "https://github.com/libexpat/libexpat/releases/download/R_${EXPAT_VERSION//./_}/expat-${EXPAT_VERSION}.tar.gz"
-tar xf "expat-${EXPAT_VERSION}.tar.gz"
-cd "expat-${EXPAT_VERSION}"
-cmake -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DBUILD_SHARED_LIBS=OFF
-cmake --build build
-cmake --install build
-cd ..
+mkdir -p expat-src
+curl -sL "https://github.com/libexpat/libexpat/releases/download/R_${EXPAT_VERSION//./_}/expat-${EXPAT_VERSION}.tar.gz" | tar xfz - --strip=1 -C expat-src
+cmake -S expat-src  -Bbuild-expat -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DBUILD_SHARED_LIBS=OFF
+cmake --build build-expat
+cmake --install build-expat
 
 echo "==> Building WolfSSL with OpenSSL compatibility..."
-curl -LO "https://github.com/wolfSSL/wolfssl/archive/refs/tags/v${WOLFSSL_VERSION}-stable.tar.gz"
-tar xf "v${WOLFSSL_VERSION}-stable.tar.gz"
-cd "wolfssl-${WOLFSSL_VERSION}-stable"
-cmake -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DBUILD_SHARED_LIBS=OFF -DWOLFSSL_CURL=ON
-cmake --build build
-cmake --install build
-cd ..
+mkdir -p wolfssl-src
+curl -sL "https://github.com/wolfSSL/wolfssl/archive/refs/tags/v${WOLFSSL_VERSION}-stable.tar.gz" | tar xfz - --strip=1 -C wolfssl-src
+cmake -Swolfssl-src -Bbuild-wolfssl -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DBUILD_SHARED_LIBS=OFF -DWOLFSSL_CURL=ON
+cmake --build build-wolfssl
+cmake --install build-wolfssl
 
 echo "==> Building curl with WolfSSL and static libraries..."
-curl -LO "https://curl.se/download/curl-${CURL_VERSION}.tar.gz"
-tar xf "curl-${CURL_VERSION}.tar.gz"
-cd "curl-${CURL_VERSION}"
-cmake -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DBUILD_SHARED_LIBS=OFF -DCURL_USE_LIBPSL=OFF -DCURL_USE_WOLFSSL=ON
-cmake --build build
-cmake --install build
-cd ..
+mkdir -p curl-src
+curl -sL "https://curl.se/download/curl-${CURL_VERSION}.tar.gz" | tar xfz - --strip=1 -C curl-src
+cmake  -Scurl-src -Bbuild-curl -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DBUILD_SHARED_LIBS=OFF -DCURL_USE_LIBPSL=OFF -DCURL_USE_WOLFSSL=ON
+# cmake  -Scurl-src -Bbuild-curl -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DBUILD_SHARED_LIBS=OFF -DCURL_USE_LIBPSL=OFF -DCURL_USE_WOLFSSL=ON -DCURL_USE_SECTRANSP=ON
+cmake --build build-curl
+cmake --install build-curl
 
 echo "==> Building Git statically with curl, zlib, expat, and WolfSSL..."
-curl -LO "https://github.com/git/git/archive/refs/tags/v${GIT_VERSION}.tar.gz"
-tar xf "v${GIT_VERSION}.tar.gz"
-cd "git-${GIT_VERSION}"
+mkdir -p git-src
+curl -sL "https://github.com/git/git/archive/refs/tags/v${GIT_VERSION}.tar.gz" | tar xfz - --strip=1 -C git-src
+cd git-src
 
 # TODO: make install target correct
-make -j"$NUM_CPUS" NO_TCLTK=YesPlease NO_GETTEXT=YesPlease NO_OPENSSL=YesPlease \
+make -j6 NO_TCLTK=YesPlease NO_GETTEXT=YesPlease NO_OPENSSL=YesPlease \
   CURLDIR="$INSTALL_DIR" ZLIB_PATH="$INSTALL_DIR" EXPAT_PATH="$INSTALL_DIR" \
   CURL_LDFLAGS="-L$INSTALL_DIR/install/lib -lcurl -lwolfssl -lm" DESTDIR="$INSTALL_DIR" install
 
